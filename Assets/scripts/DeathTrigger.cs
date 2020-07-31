fc70095a60e5997824d7debdc4acccbc227ff422
DESKTOP-8I0FRJL\jesus <Ferniz>
2020-07-30 23:34:05 -0500

    DeathTrigger / Materials

---
+++ Assets/scripts/DeathTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.GameOver();
+    }
+}
+
